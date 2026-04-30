acfc0a620837a1e05758789562887d0877538fdc
tai-shis <tshis070@mtroyal.ca>
2026-04-29 19:55:57 -0600

    fix: typo

--- app/components/projects/stoa.tsx
+++ app/components/projects/stoa.tsx
@@ -4,7 +4,7 @@ export default function Stoa() {
   return (
     <ProjectTemplate
       panelName="stoa"
-      description="A private communication platform build with a focus on privacy."
+      description="A private communication platform built with a focus on privacy."
       tags={["TypeScript", "React", "Convex", "Rust", "Tauri", "Cloudflare R2"]}
       link="https://stoa.gg"
       personal={true}
